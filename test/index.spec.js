4ab88dd8ee83d34b8071a94c3ab4ef1d61e059b4
Tom McKenzie <tom@chillidonut.com>
2018-10-23 23:33:03 +1100

    Adds named import unit test

---
+++ test/index.spec.js
@@ -0,0 +1,37 @@
+const assert = require('assert').strict
+
+const path = require('path')
+const fs = require('fs')
+
+const ts = require('typescript');
+const importsTransformer = require('../transformer').default;
+
+describe('Resolves baseUrl', () => {
+  const COMPILE_OPTIONS = {
+    "strict": true,
+    "noEmitOnError": true,
+    "baseUrl": "test/baseDir",
+    "declaration": true
+  }
+  const FILE = path.resolve(__dirname, 'named-base.ts')
+  const program = ts.createProgram([FILE], COMPILE_OPTIONS);
+
+  it('should transform named import', (done) => {
+    const callback = (filename, compiledSource) => {
+      if (filename.endsWith('named-base.js')) {
+        assert.ok(
+          compiledSource.match(/require\("\.\/baseDir\/isTruthy"\);/g),
+          'Correctly rewrites named import'
+        )
+        done()
+      }
+    }
+
+    const transformers = {
+      before: [importsTransformer(program)],
+    }
+    const { emitSkipped, diagnostics } = program.emit(undefined, callback, undefined, false, transformers);
+
+    assert.equal(emitSkipped, false, diagnostics.map(diagnostic => diagnostic.messageText).join('\n'))
+  })
+})
