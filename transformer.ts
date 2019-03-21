e3e00b2355bea6f212730e872ce8f14cd200c16e
Tom McKenzie <tom@chillidonut.com>
2019-03-21 17:09:14 +1100

    Use tsutil's findImportLikeNodes to visit more node types

--- transformer.ts
+++ transformer.ts
@@ -1,9 +1,11 @@
+import { findImportLikeNodes, ImportKind , ImportLike, isLiteralTypeNode } from 'tsutils'
 import * as ts from 'typescript'
 
 import * as nodePath from 'path';
 import { createMatchPath, MatchPath } from 'tsconfig-paths';
 
-// ugly hack because passing TSCONFIG_PATH_EXTENSIONS to tsconfig-path doens't really work?
+// ugly hack because passing TSCONFIG_PATH_EXTENSIONS to tsconfig-path doens't really work in plain node
+// https://github.com/dividab/tsconfig-paths/issues/61
 require.extensions['.ts'] = require.extensions['.js']
 require.extensions['.tsx'] = require.extensions['.js']
 const TSCONFIG_PATH_EXTENSIONS = ['.ts', '.tsx']
@@ -15,7 +17,7 @@ const TSCONFIG_PATH_EXTENSIONS = ['.ts', '.tsx']
 
 const transform = (program: ts.Program) => transformerFactory
 
-type ImportExportNode = ts.ExportDeclaration | ts.ImportDeclaration
+type AssertNever<T extends never> = T;
 
 const transformerFactory: ts.TransformerFactory<ts.SourceFile> = context => {
   const compilerOptions = context.getCompilerOptions()
@@ -26,68 +28,71 @@ const transformerFactory: ts.TransformerFactory<ts.SourceFile> = context => {
 };
 
 function visitSourceFile(sourceFile: ts.SourceFile, context: ts.TransformationContext, matchPathFunc: MatchPath) {
-  return visitNodeAndChildren(sourceFile);
+  const sourceFilePath = nodePath.dirname(sourceFile.fileName)
 
-  function visitNodeAndChildren(node: ts.Node): ts.Node {
-    if (node == null) {
-      return node;
-    }
-
-    node = visitNode(node);
-
-    return ts.visitEachChild(node, childNode => visitNodeAndChildren(childNode), context);
+  for (const node of findImportLikeNodes(sourceFile, ImportKind.All)) {
+    visitNode(node)
   }
 
-  function visitNode(node: ts.Node) {
-    if (ts.isExportDeclaration(node) || ts.isImportDeclaration(node)) {
-      return visitImportExportNode(node)
+  return sourceFile;
+
+  function visitNode(node: ImportLike) {
+    // This is similar to tsutil's findImports, modified for our use case
+    // https://github.com/ajafff/tsutils/blob/f14d5ad2edef34dbf38d08d579d7db4c33d2f55d/util/util.ts#L1131
+    switch (node.kind) {
+      case ts.SyntaxKind.ImportDeclaration: { // node.moduleSpecifier
+          if (!node.moduleSpecifier.getText() || isModuleSpecifierRelative(node.moduleSpecifier)) {
+            return node
+          }
+
+          const specifierValue = getModuleSpecifierValue(node.moduleSpecifier)
+          const matchedPath = matchPathFunc(specifierValue)
+
+          if (matchedPath) {
+            node.moduleSpecifier = createRelativeLiteral(matchedPath)
+          }
+          break;
+      }
+      case ts.SyntaxKind.ImportEqualsDeclaration:
+          // node.moduleReference.expression
+          break;
+      case ts.SyntaxKind.ExportDeclaration:
+          // node.moduleSpecifier
+          break;
+      case ts.SyntaxKind.CallExpression:
+          // node.arguments[0]
+          break;
+      case ts.SyntaxKind.ImportType:
+          // if (isLiteralTypeNode(node.argument)) {
+          //     // visitNode(node.argument.literal);
+          // }
+          break;
+      default:
+          throw new Error('Unexpected ImportLike node') as AssertNever<typeof node>;
     }
-    return node;
+    return node
   }
 
-  function visitImportExportNode(node: ImportExportNode) {
-    if (!node.moduleSpecifier || isImportExportSpecifierRelative(node)) {
-      return node
-    }
-
-    // const sourceFilePath = sourceFile.fileName
-    const sourceFilePath = nodePath.dirname(sourceFile.fileName)
-    return relativizeImportExportNode(node, sourceFilePath)
+  function createRelativeLiteral(path: string): ts.StringLiteral {
+    const replacePath = nodePath.relative(sourceFilePath, path)
+    return ts.createLiteral(isPathRelative(replacePath) ? replacePath : `./${replacePath}`)
   }
 
-  function isImportExportSpecifierRelative(node: ImportExportNode) {
-    if (node.moduleSpecifier && node.moduleSpecifier.getSourceFile()) {
-      return isPathRelative(getModuleSpecifierValue(node.moduleSpecifier))
+  function isModuleSpecifierRelative(moduleSpecifier: ts.Expression) {
+    if (moduleSpecifier && moduleSpecifier.getSourceFile()) {
+      return isPathRelative(moduleSpecifier.getText())
     }
     return false
-  }
-
-  function getModuleSpecifierValue(specifier: ts.Expression) {
-    // it's hard, so we'll just assume leading width is the length of the trailing width
-    const value = specifier.getText().substr(specifier.getLeadingTriviaWidth(), specifier.getWidth() - specifier.getLeadingTriviaWidth() * 2)
-    return value
   }
 
   function isPathRelative(path: string) {
     return (path.startsWith('./') || path.startsWith('../'))
   }
 
-  function relativizeImportExportNode(node: ImportExportNode, sourceFilePath: string) {
-    if (!node.moduleSpecifier || !node.moduleSpecifier.getSourceFile()) {
-      return node
-    }
-
-    const specifierValue = getModuleSpecifierValue(node.moduleSpecifier)
-    const matchedPath = matchPathFunc(specifierValue)
-
-    if (matchedPath) {
-      const replacePath = nodePath.relative(sourceFilePath, matchedPath)
-      // replace the module specifier
-      node.moduleSpecifier = ts.createLiteral(isPathRelative(replacePath) ? replacePath : `./${replacePath}`)
-    } else {
-    }
-
-    return node
+  function getModuleSpecifierValue(specifier: ts.Expression) {
+    // it's hard, so we'll just assume leading width is the length of the trailing width
+    const value = specifier.getText().substr(specifier.getLeadingTriviaWidth(), specifier.getWidth() - specifier.getLeadingTriviaWidth() * 2)
+    return value
   }
 }
 
